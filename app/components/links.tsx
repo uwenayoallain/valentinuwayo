cba22c070b2bf3bec85f54643b9d916ee5fd67a7
UWENAYO Alain Pacifique <uwenayoallain@gmail.com>
2023-12-30 09:44:51 +0200

    Update component styles and add resume button

--- app/components/links.tsx
+++ app/components/links.tsx
@@ -3,6 +3,9 @@
 import Link from "next/link";
 import clsx from "clsx";
 import Image from "next/image";
+import { motion } from "framer-motion";
+import { useState } from "react";
+import { Button } from "./components";
 
 export default function NavLinks() {
   const links = [
@@ -12,25 +15,47 @@ export default function NavLinks() {
     { href: "#dashboards", label: "Dashboards" },
     { href: "#contact", label: "Contact" },
   ];
+  const [isselected, setIsselected] = useState<string>(links[0].href);
   return (
-    <nav className='sticky top-0 z-10'>
+    <nav className='sticky top-0 z-10 bg-white'>
       <ul className='grid justify-between grid-cols-2 px-8 py-2'>
         <li>
           <Link href='/' className='text-xl font-bold text-white no-underline'>
             <Image src={"/logo.jpeg"} alt='logo' width={48} height={48} />
           </Link>
         </li>
-        <ul className='flex items-center justify-start space-x-4'>
-          {links.map(({ href, label }) => (
-            <li key={`${href}${label}`}>
-              <Link
-                href={href}
-                className={clsx("text-gray-950 cursor-pointer no-underline")}>
-                {label}
-              </Link>
-            </li>
-          ))}
-        </ul>
+        <div className='flex'>
+          <ul
+            className='flex items-center justify-start space-x-4 text-sm'
+            onMouseLeave={() =>
+              setTimeout(() => setIsselected(links[0].href), 300)
+            }>
+            {links.map(({ href, label }) => (
+              <li
+                key={`${href}${label}`}
+                onMouseOver={() => setTimeout(() => setIsselected(href), 200)}
+                className='px-4 py-2 relative cursor-pointer'>
+                {isselected == href ? (
+                  <motion.div
+                    className='absolute inset-0 bg-gray-100 -z-[1]'
+                    layoutId='linkbackground'></motion.div>
+                ) : null}
+                <Link
+                  href={href}
+                  className={clsx(
+                    "text-gray-950 cursor-pointer no-underline relative"
+                  )}>
+                  {label}
+                </Link>
+              </li>
+            ))}
+          </ul>
+          <li className='ml-2'>
+            <Link href='/resume.pdf' target='_blank'>
+              <Button>Resume</Button>
+            </Link>
+          </li>
+        </div>
       </ul>
     </nav>
   );
